0d255ad7371a1948381536ae9d1915f994d4b66b
Jye Lewis <jye@jyelewis.com>
2025-12-04 16:13:30 +1100

    Day 4

--- 04/04.ts
+++ 04/04.ts
@@ -1,37 +1,34 @@
 import { Direction, Grid } from "../utilities";
 
 export function day04a(input: string) {
-  const grid = Grid.fromString(input);
-
-  return grid.positions
-    .filter((pos) => pos.value === "@")
-    .count((pos) => {
-      const hasFewerThan4 = pos.adjacents(Direction.ALL).filter((x) => x.value === "@").length < 4;
-      return hasFewerThan4;
-    });
+  return (
+    Grid.fromString(input)
+      .positions.filter((pos) => pos.value === "@") // filter to only positions with paper (@)
+      // count any positions that have less than 4 adjacent paper (@) positions
+      .count((pos) => pos.adjacents(Direction.ALL).filter((x) => x.value === "@").length < 4)
+  );
 }
 
 export function day04b(input: string) {
   const grid = Grid.fromString(input);
-
   let totalRemoved = 0;
 
   while (true) {
+    // find any "@" positions that have less than 4 adjacent "@" positions - we can remove those
     const removablePositions = grid.positions
       .filter((pos) => pos.value === "@")
-      .filter((pos) => {
-        const hasFewerThan4 = pos.adjacents(Direction.ALL).filter((x) => x.value === "@").length < 4;
-        return hasFewerThan4;
-      });
+      .filter((pos) => pos.adjacents(Direction.ALL).filter((x) => x.value === "@").length < 4);
+
+    // mark them as removed
+    removablePositions.forEach((pos) => (pos.value = "#"));
+
+    // keep track of how many we removed
     totalRemoved += removablePositions.length;
 
+    // if we can't remove any more, we're done
     if (removablePositions.length === 0) {
       break;
     }
-
-    removablePositions.forEach((pos) => {
-      grid.setValue(pos, "x");
-    });
   }
 
   return totalRemoved;
