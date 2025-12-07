64181d4e1da6307d1880f9a553db7ede4fff62c1
Jye Lewis <jye@jyelewis.com>
2025-12-07 16:11:52 +1100

    Day 7 - untidied solution

---
+++ 07/07.test.ts
@@ -0,0 +1,29 @@
+import { describe, it } from "node:test";
+import fs from "fs";
+import assert from "node:assert";
+import { day07a, day07b } from "./07";
+
+describe("day07", () => {
+  const sampleInput = fs.readFileSync("07/example-input.txt").toString("utf-8");
+  const input = fs.readFileSync("07/input.txt").toString("utf-8");
+
+  it("07a sample input", () => {
+    const answer = day07a(sampleInput);
+    assert.equal(answer, 21);
+  });
+
+  it("07a input", () => {
+    const answer = day07a(input);
+    assert.equal(answer, 1602);
+  });
+
+  it("07b sample input", () => {
+    const answer = day07b(sampleInput);
+    assert.equal(answer, 123);
+  });
+
+  it("07b input", () => {
+    const answer = day07b(input);
+    assert.equal(answer, 135656430050438);
+  });
+});
---
+++ 07/07.ts
@@ -0,0 +1,51 @@
+import { Grid, GridPosition, memo } from "../utilities";
+
+export function day07a(input: string) {
+  const grid = Grid.fromString(input);
+  const startPosition = grid.positions.findOne((pos) => pos.value === "S");
+
+  let splitLocations = new Set<string>();
+  let prevTachyonBeams = [startPosition];
+  while (prevTachyonBeams.length > 0) {
+    const newTachyonBeams = [];
+    for (const beam of prevTachyonBeams) {
+      if (beam.downOrNull()?.value === "^") {
+        // split
+        if (!splitLocations.has(beam.down().key)) {
+          splitLocations.add(beam.down().key);
+          newTachyonBeams.push(beam.down().right());
+          newTachyonBeams.push(beam.down().left());
+        }
+      }
+
+      if (beam.downOrNull()?.value === ".") {
+        // move down
+        newTachyonBeams.push(beam.down());
+      }
+    }
+
+    prevTachyonBeams = newTachyonBeams;
+  }
+
+  return splitLocations.size;
+}
+
+export function day07b(input: string) {
+  const grid = Grid.fromString(input);
+  const startPosition = grid.positions.findOne((pos) => pos.value === "S");
+
+  const numTimelinesFromPosition = memo((beam: GridPosition<string>): number => {
+    if (beam.downOrNull()?.value === "^") {
+      return numTimelinesFromPosition(beam.down().right()) + numTimelinesFromPosition(beam.down().left());
+    }
+
+    if (beam.downOrNull()?.value === ".") {
+      // move down
+      return numTimelinesFromPosition(beam.down());
+    }
+
+    return 1;
+  });
+
+  return numTimelinesFromPosition(startPosition);
+}
