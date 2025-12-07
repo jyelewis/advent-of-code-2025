4b225ca75934b77e69346ddabe40b7ca735c6295
Jye Lewis <jye@jyelewis.com>
2025-12-07 16:22:38 +1100

    Day 7 - replace ifs with switch

--- 07/07.ts
+++ 07/07.ts
@@ -7,21 +7,23 @@ export function day07(input: string) {
 
   // recursively count the number of unique timelines from a given position
   const numTimelinesFromPosition = memo((beam: GridPosition<string>): number => {
-    if (beam.downOrNull()?.value === "^") {
-      // part A: record unique split locations
-      splitLocations.add(beam.down().key);
+    // make one step downwards, check what we find
+    switch (beam.downOrNull()?.value) {
+      case "^": {
+        // part A: record unique split locations
+        splitLocations.add(beam.down().key);
 
-      // part B: split path, count timelines from both sides
-      return numTimelinesFromPosition(beam.down().right()) + numTimelinesFromPosition(beam.down().left());
+        // part B: split path, count timelines from both sides
+        return numTimelinesFromPosition(beam.down().right()) + numTimelinesFromPosition(beam.down().left());
+      }
+      case ".": {
+        // count timelines from moving down one
+        return numTimelinesFromPosition(beam.down());
+      }
+      default:
+        // we've reached the end of a unique timeline
+        return 1;
     }
-
-    if (beam.downOrNull()?.value === ".") {
-      // count timelines from moving down one
-      return numTimelinesFromPosition(beam.down());
-    }
-
-    // we've reached the end of a unique timeline
-    return 1;
   });
 
   const partB = numTimelinesFromPosition(startPosition);
