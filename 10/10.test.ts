2075b71deb7ca928dae88605754a981438faf8b8
Jye Lewis <jye@jyelewis.com>
2025-12-10 16:39:28 +1100

    Day 10 - Part a

---
+++ 10/10.test.ts
@@ -0,0 +1,29 @@
+import { describe, it } from "node:test";
+import fs from "fs";
+import assert from "node:assert";
+import { day10a, day10b } from "./10";
+
+describe("day10", () => {
+  const sampleInput = fs.readFileSync("10/example-input.txt").toString("utf-8");
+  const input = fs.readFileSync("10/input.txt").toString("utf-8");
+
+  it("10a sample input", () => {
+    const answer = day10a(sampleInput);
+    assert.equal(answer, 7);
+  });
+
+  it("10a input", () => {
+    const answer = day10a(input);
+    assert.equal(answer, 477);
+  });
+
+  it("10b sample input", () => {
+    const answer = day10b(sampleInput);
+    assert.equal(answer, 123);
+  });
+
+  it("10b input", () => {
+    const answer = day10b(input);
+    assert.equal(answer, 123);
+  });
+});
